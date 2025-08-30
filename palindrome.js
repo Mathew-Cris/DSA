7dbe4067a34e54ecc1aa30b98331dbc5739bb8cc
Mathew Cris <cname9306@gmail.com>
2025-08-31 02:40:51 +0800

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,23 @@
+function checker(word) {
+  // converts string to array form using .split() method
+  let name = word.split("");
+  // empty array
+  let reversedName = [];
+  // empty variable
+  let converted;
+  // loop for iterating all elements
+  for (var i = 0; i < word.length; i++) {
+    // pop all elements out then store on letters varible
+    let letters = name.pop();
+    // push each letters to the reversedName variable
+    reversedName.push(letters);
+    // converts back the array form to string
+    converted = reversedName.join("");
+  }
+  // ternary operator
+  let result =
+    word == converted
+      ? `${word} is a palindrome`
+      : `${word}  is not a palindrome`;
+  console.log(result);
+}
